6701af8ada01682b00e69bb0e1b97d1f5bbac72d
dvelazquez <dvelazquez@dvelopment.tech>
2025-07-09 05:56:53 -0700

    CosmosEmulator Middleware

--- CosmosDB/Services/CosmosDBEmulator.Services/AddPersonas.cs
+++ CosmosDB/Services/CosmosDBEmulator.Services/AddPersonas.cs
@@ -1,26 +1,22 @@
 ﻿using CosmosDBEmulator.Contracts;
 using Microsoft.Azure.Cosmos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CosmosDBEmulator.Services
 {
     public class AddPersonas : IAddPersonas
     {
-        public void AddPersona(Persona persona)
+        public async Task AddPersonaAsync(Persona persona)
         {
             try
             {
-                using (CosmosClient client = new CosmosClient(Endpoint, PrimaryKey))
+                string endpoint = "";
+                string primaryKey = "";
+                using (CosmosClient client = new CosmosClient(endpoint, primaryKey))
                 {
                     Database database = await client.CreateDatabaseIfNotExistsAsync("Personas");
                     Container container = await database.CreateContainerIfNotExistsAsync("Persona", "/ExternalId");
                     //var collection = client.CreateDocumentCollectionIfNotExistsAsync(database.SelfLink, new DocumentCollection { Id = "Persona" }).Result.Resource;
-                    var persona = new Persona
+                    var nuevaPersona = new Persona
                     {
                         Id = Guid.NewGuid().ToString(),
                         ExternalId = "123",
@@ -33,9 +29,9 @@ namespace CosmosDBEmulator.Services
                         //    Description = "Admin role"
                         //}
                     };
-                    if (!string.IsNullOrEmpty(persona.Id))
+                    if (!string.IsNullOrEmpty(nuevaPersona.Id))
                     {
-                        await container.CreateItemAsync(persona, new PartitionKey(persona.PartitionKey));
+                        await container.CreateItemAsync(nuevaPersona, new PartitionKey(nuevaPersona.PartitionKey));
                     }
                 }
             }
@@ -45,11 +41,6 @@ namespace CosmosDBEmulator.Services
                 Console.WriteLine($"Message: {ex.Message}");
                 Console.WriteLine($"Stack Trace: {ex.StackTrace}");
             }
-        }
-
-        public Task AddPersonaAsync(Persona persona)
-        {
-            throw new NotImplementedException();
         }
     }
 }
