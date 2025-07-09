f5e73432f49e22d750e63e12b033e84dc389267e
Daniel <daniel.velazquez00@gmail.com>
2025-07-09 05:39:58 -0700

    Fabrica de ordenamiento (#5)
    * Estructura de Datos init
    
    * Interfaces de Algoritmos Sort
    
    * Factory Method
    
    * Actualizacion a readme
    
    * Agregando Patrones de diseño - circuit breaker
    
    * Reparacion de codigo para remover errores de compilacion.
    Se quita seguimiento de archivos innecesaria.
    
    ---------
    
    Signed-off-by: Daniel <daniel.velazquez00@gmail.com>
    Co-authored-by: dvelazquez <dvelazquez@dvelopment.tech>

--- CosmosDB/Services/CosmosDBEmulator.Services/AddPersonas.cs
+++ CosmosDB/Services/CosmosDBEmulator.Services/AddPersonas.cs
@@ -13,38 +13,38 @@ namespace CosmosDBEmulator.Services
     {
         public void AddPersona(Persona persona)
         {
-            try
-            {
-                //using (CosmosClient client = new CosmosClient(Endpoint, PrimaryKey))
-                //{
-                //    Database database = await client.CreateDatabaseIfNotExistsAsync("Personas");
-                //    Container container = await database.CreateContainerIfNotExistsAsync("Persona", "/ExternalId");
-                //    //var collection = client.CreateDocumentCollectionIfNotExistsAsync(database.SelfLink, new DocumentCollection { Id = "Persona" }).Result.Resource;
-                //    var persona = new Persona
-                //    {
-                //        Id = Guid.NewGuid().ToString(),
-                //        ExternalId = "123",
-                //        Name = "John",
-                //        LastName = "Doe",
-                //        //AssignedRole = new Roles
-                //        //{
-                //        //    Id = Guid.NewGuid(),
-                //        //    Name = "Admin",
-                //        //    Description = "Admin role"
-                //        //}
-                //    };
-                //    if (!string.IsNullOrEmpty(persona.Id))
-                //    {
-                //        await container.CreateItemAsync(persona, new PartitionKey(persona.PartitionKey));
-                //    }
-                //}
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception Type: {ex.GetType().Name}");
-                Console.WriteLine($"Message: {ex.Message}");
-                Console.WriteLine($"Stack Trace: {ex.StackTrace}");
-            }
+            //try
+            //{
+            //    //using (CosmosClient client = new CosmosClient(Endpoint, PrimaryKey))
+            //    //{
+            //    //    Database database = await client.CreateDatabaseIfNotExistsAsync("Personas");
+            //    //    Container container = await database.CreateContainerIfNotExistsAsync("Persona", "/ExternalId");
+            //    //    //var collection = client.CreateDocumentCollectionIfNotExistsAsync(database.SelfLink, new DocumentCollection { Id = "Persona" }).Result.Resource;
+            //    //    var persona = new Persona
+            //    //    {
+            //    //        Id = Guid.NewGuid().ToString(),
+            //    //        ExternalId = "123",
+            //    //        Name = "John",
+            //    //        LastName = "Doe",
+            //    //        //AssignedRole = new Roles
+            //    //        //{
+            //    //        //    Id = Guid.NewGuid(),
+            //    //        //    Name = "Admin",
+            //    //        //    Description = "Admin role"
+            //    //        //}
+            //    //    };
+            //    //    if (!string.IsNullOrEmpty(persona.Id))
+            //    //    {
+            //    //        await container.CreateItemAsync(persona, new PartitionKey(persona.PartitionKey));
+            //    //    }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Exception Type: {ex.GetType().Name}");
+            //    Console.WriteLine($"Message: {ex.Message}");
+            //    Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+            //}
         }
 
         public Task AddPersonaAsync(Persona persona)
---
+++ EstructuraDeDatos/PlayGround.EstructuraDeDatos.Algoritmos/Program.cs
@@ -0,0 +1,20 @@
+﻿using PlayGround.EstructuraDeDatos.Algoritmos.Fabrica;
+
+namespace PlayGround.EstructuraDeDatos.Algoritmos
+{
+    internal class Program
+    {
+        // TODO: Add Timer to measure the time it takes to sort the array
+        // TODO: Implement method foreach sorting algorithm
+        static void Main(string[] args)
+        {
+            // BubbleSort
+            IBaseFabricaOrdenamiento baseFabricaOrdenamiento = FabricaOrdenamiento.CrearOrdenamiento(TiposFabricaOrdenamiento.BubbleSort);
+            int[] arreglo = { 1, 2, 3, 4, 5 };
+            
+            baseFabricaOrdenamiento.Sort(arreglo);
+
+            Console.WriteLine(string.Join(", ", arreglo));
+        }
+    }
+}
