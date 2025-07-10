7cc59cf78f69f480668d1ce3766fdd73903a60fa
dvelazquez <dvelazquez@dvelopment.tech>
2025-07-09 20:06:01 -0700

    HttpClient registration in the program or startup file

---
+++ dv.autenticacion.auth0/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dv.autenticacion.auth0
+{
+    internal class Program
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration config)
+        {
+            services.AddHttpClient("PlayersApi", (serviceProvider, client) =>
+            {
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+                var section = config.GetSection("HttpClientSettings:PlayersApi");
+                client.BaseAddress = new Uri(section.GetValue<string>("BaseAddress"));
+                client.Timeout = TimeSpan.FromSeconds(section.GetValue<int>("TimeoutSeconds"));
+            });
+            services.AddHttpClient("TransactionsApi", (serviceProvider, client) =>
+            {
+                var config = serviceProvider.GetRequiredService<IConfiguration>();
+                var section = config.GetSection("HttpClientSettings:TransactionsApi");
+                client.BaseAddress = new Uri(section.GetValue<string>("BaseAddress"));
+                client.Timeout = TimeSpan.FromSeconds(section.GetValue<int>("TimeoutSeconds"));
+            });
+        }
+    }
+}
