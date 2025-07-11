97bde4825e39e5ed7bd9c7002c18d64f5098c826
dvelazquez <daniel.velazquez00@gmail.com>
2025-07-11 11:11:11 -0700

    Formulario

---
+++ Blazor/BlazorApp1/Modelos/FormularioSimple.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorApp1.Modelos
+{
+    public class FormularioSimple
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        //public string Telefono { get; set; } = string.Empty;
+        //public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+        //public bool AceptaTerminos { get; set; } = false;
+        public FormularioSimple() { }
+
+        public FormularioSimple(string nombre, string apellido, string correoElectronico)//, string telefono, DateTime fechaNacimiento, bool aceptaTerminos)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            CorreoElectronico = correoElectronico;
+            //Telefono = telefono;
+            //FechaNacimiento = fechaNacimiento;
+            //AceptaTerminos = aceptaTerminos;
+        }
+    }
+}
