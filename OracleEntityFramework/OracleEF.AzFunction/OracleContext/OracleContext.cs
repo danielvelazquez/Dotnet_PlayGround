b152654c7656ed0bfea3c8182790fbed5ed22632
dvelazquez <dvelazquez@dvelopment.tech>
2025-08-01 13:03:43 -0700

    Oracle Entity Framework

---
+++ OracleEntityFramework/OracleEF.AzFunction/OracleContext/OracleContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleEF.AzFunction.OracleContext
+{
+    public class OracleContext : DbContext
+    {
+        public OracleContext(DbContextOptions<OracleContext> options)
+            : base(options)
+        {
+        }
+        // Define DbSets for your entities here
+        // public DbSet<YourEntity> YourEntities { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity mappings here
+        }
+    }
+}
